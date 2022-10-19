2607bc3c35d808af1b3052901c2f884e767e230c
albscr <albacecilia10@gmail.com>
2022-10-19 07:00:40 -0500

    Updates

---
+++ precache-manifest.79f63cc8bc3e6d15e05ec997813a5ef7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7644ad46514ee48f5d93",
+    "url": "/Travell-Landingpage-React/static/css/main.0b2b8fed.chunk.css"
+  },
+  {
+    "revision": "7644ad46514ee48f5d93",
+    "url": "/Travell-Landingpage-React/static/js/main.7644ad46.chunk.js"
+  },
+  {
+    "revision": "271d1efb744527e0afa6",
+    "url": "/Travell-Landingpage-React/static/js/1.271d1efb.chunk.js"
+  },
+  {
+    "revision": "93fe513254e1d5cced6a",
+    "url": "/Travell-Landingpage-React/static/js/2.93fe5132.chunk.js"
+  },
+  {
+    "revision": "f9b61f7b80048440fa01",
+    "url": "/Travell-Landingpage-React/static/js/runtime~main.f9b61f7b.js"
+  },
+  {
+    "revision": "ab32778d23109a7b5f778e4eca649677",
+    "url": "/Travell-Landingpage-React/static/media/travel02.ab32778d.jpg"
+  },
+  {
+    "revision": "3aa0d63850e78a81b54525f16f693d16",
+    "url": "/Travell-Landingpage-React/static/media/travel03.3aa0d638.jpg"
+  },
+  {
+    "revision": "20bbbb087fe319569e47cc9a83aa97c5",
+    "url": "/Travell-Landingpage-React/static/media/travel01.20bbbb08.jpg"
+  },
+  {
+    "revision": "2048f231344b4c45188f4fc08295f4a9",
+    "url": "/Travell-Landingpage-React/index.html"
+  }
+];
